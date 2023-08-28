cb5ad13ab9f20f77b3be321f3587c6791558777d
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-08-28 03:22:11 +0300

    React-Projects
    jira app

--- jira-app/src/App.jsx
+++ jira-app/src/App.jsx
@@ -1,30 +1,56 @@
 import TaskCreate from '../components/taskCreate'
 import TaskList from '../components/taskList'
 import './App.css'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
   const [tasks, setTasks] = useState([])
 
-  const createTask = (title,taskDesc) => {
+  const createTask = async (title,taskDesc) => {
+  const response = await axios.post('http://localhost:3000/tasks', {
+      title,
+      taskDesc
+    })
+
     const createdTasks = [
+      ...tasks, 
+      response.data
+    ]
+
+  /*   const createdTasks = [
       ...tasks, {
         id: Math.round(Math.random() * 999999),
         title,
         taskDesc
-    }]
+    }] */
 
     setTasks(createdTasks)
 }
 
-const deleteTaskById = (id) => {
+const fetchTask = async () => {
+  const response = await axios.get('http://localhost:3000/tasks')
+  setTasks(response.data)
+}
+
+useEffect(() => {
+  fetchTask()
+},[])
+  
+
+const deleteTaskById = async (id) => {
+  await axios.delete(`http://localhost:3000/tasks/${id}`)
   const afterDeletingTasks = tasks.filter((task) => {
     return task.id !== id
   })
   setTasks(afterDeletingTasks)
 }
 
-const editTaskById = (id,UpdatedTitle,updatedDesc) => {
+const editTaskById = async (id,UpdatedTitle,updatedDesc) => {
+  await axios.put(`http://localhost:3000/tasks/${id}`,{
+    title: UpdatedTitle,
+    taskDesc: updatedDesc
+  })
   const updatedTasks = tasks.map((task) => {
     if(task.id === id){
       return {id,
