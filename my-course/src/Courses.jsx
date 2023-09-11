b34a633f0025418d3d6c44b5c317691fead16978
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-09-12 01:12:47 +0300

    React-Projects

--- my-course/src/Courses.jsx
+++ my-course/src/Courses.jsx
@@ -1,23 +1,82 @@
 import Course from './Course'
+import { useState } from 'react'
+import {FaChevronLeft, FaChevronRight} from 'react-icons/fa'
 
 function Courses({courses,buyCourse}) {
 //console.log(courses)
+const [index, setIndex] = useState(0)
+const {content, title, price} = courses[index]
+
+console.log(index)
+
+const checkIndex = (index) => {
+  if( index < 0) {
+    return courses.length -1
+  }if(index > courses.length - 1){
+    return 0
+  }
+  return index
+}
+
+const getRandomCourse = () => {
+  Math.random() *
+  setIndex((index) => {
+    let idx = index - 1 
+    return checkIndex(idx)
+  })
+}
+
+const prevCourse = () => {
+  setIndex((index) => {
+    let idx = index - 1 
+    return checkIndex(idx)
+  })
+}
+
+const nextCourse = () => {
+  setIndex((index) => {
+    let idx = index + 1
+  return checkIndex(idx)
+  })
+}
 
   return (
     <div>
       <div  className="header">
         <h2>MY COURSES</h2>
       </div>
+    
+    <button className='random' onClick={getRandomCourse}>
+      Random Course
+    </button>
 
       <div className='courses'>
-        {
+    
+
+    <button className='icon' onClick={prevCourse}>
+      <FaChevronLeft/>
+    </button>
+
+      <div className="card">
+      <h4 className="cardTitle">{title}</h4>
+      <p className="content">{content}</p>
+      <button className="buyBtn" onClick={() => {
+      buyOneCourse(id)
+    }}>BUY {price}</button>
+
+    </div>
+
+    <button className='icon' onClick={nextCourse}>
+      <FaChevronRight/>
+    </button>
+       {/*  {
           courses.map((course) => {
             return (
               <Course key={course.id} {...course} buyOneCourse={buyCourse}/>
               //course={course}
             )
           })
-        }
+        } */}
       </div>
     </div>
   )
