e9796998710975fa9852cc02627c64d46411d5d6
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-09-26 23:30:36 +0300

    React-Project
    E-signature app

---
+++ E-signature-App/src/Esignature.jsx
@@ -0,0 +1,63 @@
+import React, { useState} from 'react'
+import './Style.css'
+import Title from './components/Title'
+
+function Esignature() {
+
+const [name, setName] = useState("")
+const [date, setDate] = useState("")
+const [text, setText] = useState("")
+
+const handleName = (e) => {
+    setName(e.target.value)
+}
+
+const handleDate = (e) => {
+    setDate(e.target.value)
+}
+
+const handleText = (e) => {
+    setText(e.target.value)
+}
+
+const inputStyle = {
+    width : "5rem",
+    height: "3rem",
+    border: "1px solid #333",
+    margin: "3rem 1rem 2rem",
+    padding: "1rem",
+    backgroundColor: "#fff",
+    color: "gray"
+}
+
+  return (
+    <>
+    <div className='container text-center'>
+
+        <h1 className='header'>E-Signature</h1>
+
+        <div className='card'>
+        <Title classes={"title"} text={name}/>
+        <Title classes={"subtitle"} text= {!date ?"Date" :date }/>
+        <p className='text'>{!text ?"Could you tell us about yourself?" :text}</p>
+        
+        </div>
+
+    <div className='input'>
+    
+    <input type="text" value={name} style= {inputStyle} placeholder= {"Name"} onChange={handleName}  />
+
+    <input type="date" value={date} style= {inputStyle} onChange= {handleDate}/>
+
+    <input type="text" value={text} style= {inputStyle} onChange= {handleText} placeholder="Text" />
+
+
+    </div>
+
+    <button type='submit' className='submitBtn'> Submit </button>
+    </div>
+    </>
+  )
+}
+
+export default Esignature
---
+++ E-signature-App/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Esignature from './Esignature.jsx'
+import App from './Esignature.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Esignature />
+)
