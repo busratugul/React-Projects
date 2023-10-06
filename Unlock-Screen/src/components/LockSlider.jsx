cf1e25f50011d02c34d18ef026524ca75a7a6590
Büşra Tuğul <134439412+busratugul@users.noreply.github.com>
2023-10-06 17:02:50 +0300

    React-Projects
    Unlock-Screen

--- Unlock-Screen/src/components/LockSlider.jsx
+++ Unlock-Screen/src/components/LockSlider.jsx
@@ -4,11 +4,11 @@ import '../App.css'
 function LockSlider({handleInput, sliderValue, width}) {
   let sliderStyle = {
     appearance: "none",
-    width: !width ?"300px" :width,
+    width: !width ?"200px" :width,
     height:"50px",
     backgroundColor: "rgba(188, 190, 188, 0.5)",
     outline: "none",
-
+    borderRadius: "45px"
 
   }
   return <input type="range" className="slider"style={sliderStyle} 
--- Unlock-Screen/src/components/SlideToUnlock.jsx
+++ Unlock-Screen/src/components/SlideToUnlock.jsx
@@ -1,11 +1,48 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import '../App.css'
 import LockSlider from './LockSlider'
 import {AiFillUnlock} from 'react-icons/ai'
+import unlockBg from '../images/unlock.jpg'
+import HomeBg from '../images/screen.jpg'
 
 function SlideToUnlock() {
+  const [uiProps, setUiProps] = useState({
+    uiText:"Unlock Screen",
+    uiColor:"#eee",
+    uiBg:`url(${unlockBg}) center/cover no-repeat`
+  })
 
-  const [showLockSlider, setShowLockSlider] = useState(false)
+  const [showLockSlider, setShowLockSlider] = useState(true)
+  const [lockSliderValue, setLockSliderValue] = useState(0)
+
+  const handleLockSliderInput = (e) => {
+    setLockSliderValue(e.target.value)
+    //console.log(e.target.value);
+  }
+
+  const lockScreen = () => {
+    setLockSliderValue(0)
+    setShowLockSlider(true)
+    setUiProps({
+      uiText:"Unlock Screen",
+      uiColor:"#eee",
+      uiBg:`url(${unlockBg}) center/cover no-repeat`
+      })
+  }
+
+  useEffect(() => {
+   document.body.style.background= "#999"
+   if(lockSliderValue === "100"){
+    setLockSliderValue(0)
+    setShowLockSlider(false)
+    setUiProps({
+      uiText:"Home Screen",
+      uiColor:"#222",
+      uiBg:`url(${HomeBg}) center/cover no-repeat`
+    })
+   }
+  },[lockSliderValue])
+  
 
   return (
     <div className='container' style={{
@@ -13,13 +50,18 @@ function SlideToUnlock() {
       margin:"15vh 25vh",
       width:340,
       border:"4px solid #000",
-      borderRadius:"15px"
+      borderRadius:"15px",
+      background: uiProps.uiBg
     }}>
 
-      <h1 className='title'>Title</h1>
+      <h1 className='title' 
+      style={{color: uiProps.uiColor}}>
+        {uiProps.uiText}
+       </h1>
       {showLockSlider 
-      ?(<LockSlider width={"250px"}/> )
-      :(<AiFillUnlock className='unlockIcon'/>)}
+      ?(<LockSlider 
+        width={"200px"} handleInput={handleLockSliderInput} value={lockSliderValue}/>)
+      :(<AiFillUnlock className='unlockIcon' onClick={lockScreen}/>)}
       
     </div>
   )
